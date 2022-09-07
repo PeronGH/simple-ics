2e41af87b7badb7787f1e5e831328c725b619c34
peron <peron_ap@icloud.com>
2022-09-07 13:46:36 +0800

    copy date.js from others

--- date.ts
+++ date.ts
@@ -1,5 +1,3 @@
-import formatDate from 'https://raw.githubusercontent.com/adamgibbons/ics/master/src/utils/format-date.js';
-
 export default function parseDate(date: DateData) {
   if (date instanceof Date) {
     date = [
@@ -16,3 +14,53 @@ export default function parseDate(date: DateData) {
 
 type DateArray = number[];
 export type DateData = DateArray | Date;
+
+// The following code is copied from https://github.com/adamgibbons/ics/blob/master/src/utils/format-date.js
+
+const pad = (n: number) => (n < 10 ? `0${n}` : `${n}`);
+
+function formatDate(
+  args: number[] = [],
+  outputType = 'utc',
+  inputType = 'local'
+) {
+  if (Array.isArray(args) && args.length === 3) {
+    const [year, month, date] = args;
+    return `${year}${pad(month)}${pad(date)}`;
+  }
+
+  let outDate = new Date(new Date().setUTCSeconds(0, 0));
+  if (Array.isArray(args) && args.length > 0 && args[0]) {
+    const [year, month, date, hours = 0, minutes = 0, seconds = 0] = args;
+    if (inputType === 'local') {
+      outDate = new Date(year, month - 1, date, hours, minutes, seconds);
+    } else {
+      outDate = new Date(
+        Date.UTC(year, month - 1, date, hours, minutes, seconds)
+      );
+    }
+  }
+
+  if (outputType === 'local') {
+    return [
+      outDate.getFullYear(),
+      pad(outDate.getMonth() + 1),
+      pad(outDate.getDate()),
+      'T',
+      pad(outDate.getHours()),
+      pad(outDate.getMinutes()),
+      pad(outDate.getSeconds()),
+    ].join('');
+  }
+
+  return [
+    outDate.getUTCFullYear(),
+    pad(outDate.getUTCMonth() + 1),
+    pad(outDate.getUTCDate()),
+    'T',
+    pad(outDate.getUTCHours()),
+    pad(outDate.getUTCMinutes()),
+    pad(outDate.getUTCSeconds()),
+    'Z',
+  ].join('');
+}
