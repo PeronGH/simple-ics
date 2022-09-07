e189fa4f2d6e62ea62c4fcd88db797f261e29448
peron <peron_ap@icloud.com>
2022-09-07 13:18:57 +0800

    refactor toString()

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
 import { crypto } from 'https://deno.land/std@0.154.0/crypto/mod.ts';
 import parseDate from './date.ts';
 import { DateData } from './date.ts';
+import { ContentLine, stringifyLines } from './stringify.ts';
 
-const calendarBegin = `BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:peron/ics
-CALSCALE:GREGORIAN
-METHOD:PUBLISH`;
+const calendarBegin: ContentLine = ['BEGIN', 'VCALENDAR'];
 
-const calendarEnd = 'END:VCALENDAR';
+const calendarProps: ContentLine[] = [
+  ['VERSION', '2.0'],
+  ['PRODID', 'peron/simple_ics'],
+  ['METHOD', 'PUBLISH'],
+];
 
-const eventBegin = 'BEGIN:VEVENT';
+const calendarEnd: ContentLine = ['END', 'VCALENDAR'];
 
-const eventEnd = 'END:VEVENT';
+const eventBegin: ContentLine = ['BEGIN', 'VEVENT'];
+
+const eventEnd: ContentLine = ['END', 'VEVENT'];
 
 export class Event {
   constructor(protected config: EventConfig) {
@@ -34,31 +37,35 @@ export class Event {
     }
   }
 
-  public toString() {
+  public toLines(): ContentLine[] {
     const uid = crypto.randomUUID();
     const { title } = this.config;
 
     const now = new Date();
 
-    return `${eventBegin}
-UID:${uid}
-DTSTAMP:${parseDate(now)}
-DTSTART:${parseDate(this.config.beginDate)}
-DTEND:${parseDate(this.config.endDate!)}
-SUMMARY:${title}
-${eventEnd}`;
+    return [
+      eventBegin,
+      ['UID', uid],
+      ['DTSTAMP', parseDate(now)],
+      ['DTSTART', parseDate(this.config.beginDate)],
+      ['DTEND', parseDate(this.config.endDate!)],
+      ['SUMMARY', title],
+      eventEnd,
+    ];
   }
 }
 
 export class Calendar {
   constructor(protected events: Event[]) {}
 
-  public toString() {
-    const eventText = this.events.map(evt => evt.toString()).join('\n');
+  toLines(): ContentLine[] {
+    const eventLines = this.events.map(evt => evt.toLines()).flat();
 
-    return `${calendarBegin}
-${eventText}
-${calendarEnd}`;
+    return [calendarBegin, ...calendarProps, ...eventLines, calendarEnd];
+  }
+
+  toString() {
+    return stringifyLines(this.toLines());
   }
 }
 
---
+++ stringify.ts
@@ -0,0 +1,10 @@
+export type ContentLine = [string, string];
+
+function stringifyLine(line: ContentLine) {
+  // TODO: Handle lines longer than 75 bytes
+  return `${line[0].toUpperCase()}:${line[1]}`;
+}
+
+export function stringifyLines(lines: ContentLine[]) {
+  return lines.map(stringifyLine).join('\r\n');
+}
