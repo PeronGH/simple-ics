9bedf74fac9a4baf61008433b415ffc5b4cd04fa
peron <peron_ap@icloud.com>
2022-09-07 19:12:27 +0800

    update byday

--- mod.ts
+++ mod.ts
@@ -1,2 +1,3 @@
 export { Calendar, Event } from './main.ts';
 export type { EventConfig } from './main.ts';
+export type { Day, RecurrenceRule } from './rrule.ts';
--- rrule.ts
+++ rrule.ts
@@ -5,20 +5,24 @@ export interface RecurrenceRule {
   until?: DateData;
   interval?: number;
   count?: number;
+  byDay?: Day[];
 }
 
 export function parseRRule(rrule: RecurrenceRule | undefined) {
   if (rrule === undefined) return;
 
-  const { freq, until, interval, count } = rrule;
+  const { freq, until, interval, count, byDay } = rrule;
 
   return [
     ['FREQ', freq],
     ['UNTIL', parseDate(until)],
     ['INTERVAL', interval],
     ['COUNT', count],
+    ['BYDAY', byDay ? byDay.join() : undefined],
   ]
     .filter(line => line[1] !== undefined)
     .map(line => `${line[0]}=${line[1]};`)
     .join('');
 }
+
+export type Day = 'MO' | 'TU' | 'WE' | 'TH' | 'FR' | 'SA' | 'SU';
